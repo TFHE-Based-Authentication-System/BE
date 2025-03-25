eaca4c954affab18d55e9a8481d3ddb8bb6eb8a0
nam <yhnham@gmail.com>
2025-03-25 17:03:24 +0900

    file upload

---
+++ src/main/java/com/tfheauth/face_auth_server/FaceAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.tfheauth.face_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfheauth/face_auth_server/FaceAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfheauth.face_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
