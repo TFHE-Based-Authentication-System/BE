e492b2f427b0c01892fb8bcd488ec10aea9656dc
nam <yhnham@gmail.com>
2025-06-13 14:40:28 +0900

    modify: 모든 연산에 mod q 적용

--- src/main/java/com/tfheauth/face_auth_server/feature/FeatureService.java
+++ src/main/java/com/tfheauth/face_auth_server/feature/FeatureService.java
@@ -40,23 +40,25 @@ public class FeatureService {
         Feature feature = featureRepository.findByUser_Email(requestDTO.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("사용자의 feature가 존재하지 않습니다."));
 
+        final BigInteger Q = BigInteger.valueOf(2).pow(120);
+
         Vector stored = feature.getVector();
 
         List<String> c1 = requestDTO.getC1();
         List<String> c2 = requestDTO.getC2();
-
         List<String> db_c1 = stored.getC1();
         List<String> db_c2 = stored.getC2();
 
         List<PolynomialCoefficientDTO> result = new ArrayList<>();
 
         for (int i = 0; i < c1.size(); i++) {
-            BigInteger tildeC1 = new BigInteger(c1.get(i)).subtract(new BigInteger(db_c1.get(i)));
-            BigInteger tildeC2 = new BigInteger(c2.get(i)).subtract(new BigInteger(db_c2.get(i)));
+            // BigInteger로 변환
+            BigInteger tildeC1 = new BigInteger(c1.get(i)).subtract(new BigInteger(db_c1.get(i))).mod(Q);
+            BigInteger tildeC2 = new BigInteger(c2.get(i)).subtract(new BigInteger(db_c2.get(i))).mod(Q);
 
-            BigInteger a = tildeC2.pow(2); // s^2 계수
-            BigInteger b = tildeC1.multiply(tildeC2).multiply(BigInteger.TWO); // s 계수
-            BigInteger c = tildeC1.pow(2); // 상수항
+            BigInteger a = tildeC2.pow(2).mod(Q);  // s^2 계수
+            BigInteger b = tildeC1.multiply(tildeC2).multiply(BigInteger.TWO).mod(Q);  // s 계수
+            BigInteger c = tildeC1.pow(2).mod(Q);  // 상수항
 
             result.add(new PolynomialCoefficientDTO(a, b, c));
         }
